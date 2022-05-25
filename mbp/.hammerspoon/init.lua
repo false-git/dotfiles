f2d0ce2f4c4add71c8300284fdb000f3112aab10
false <false@wizard-limit.net>
2022-05-25 13:56:49 +0900

    update to mba

--- mbp/.hammerspoon/init.lua
+++ mbp/.hammerspoon/init.lua
@@ -1,57 +1,67 @@
-local function keyCode(key, modifiers)
-   modifiers = modifiers or {}
-   return function()
-      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
-      hs.timer.usleep(1000)
-      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()      
-   end
-end
+local VK_JIS_EISUU = 0x66
+local VK_JIS_KANA = 0x68
+local VK_JIS_YEN = 0x5d
 
-local function remapKey(modifiers, key, keyCode)
-   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
-end
-
-local function disableAllHotkeys()
-   for k, v in pairs(hs.hotkey.getHotkeys()) do
-      v['_hk']:disable()
-   end
-end
-
-local function enableAllHotkeys()
-   for k, v in pairs(hs.hotkey.getHotkeys()) do
-      v['_hk']:enable()
-   end
-end
-
-local function handleGlobalAppEvent(name, event, app)
-   if event == hs.application.watcher.activated then
-      -- hs.alert.show(name)
-      if name ~= "iTerm2" then
-         enableAllHotkeys()
-      else
-         disableAllHotkeys()
-      end
-   end
-end
-
--- appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
--- appsWatcher:start()
-
-
-
--- カーソル移動
-remapKey({}, 'home', keyCode('a', {'ctrl'}))
-remapKey({}, 'end', keyCode('e', {'ctrl'}))
-
-kana = false
-local function handleKana()
-    return function()
-        local keycode = 104
-        hs.eventtap.event.newKeyEvent({}, keycode, true):post()
-        hs.timer.usleep(1000)
-        hs.eventtap.event.newKeyEvent({}, keycode, false):post()
-        kana = not kana
+function flagsMatches(flags, modifiers)
+    local set = {}
+    for _, k in ipairs(modifiers) do set[string.lower(k)] = true end
+    for _, k in ipairs({'fn', 'cmd', 'ctrl', 'alt', 'shift'}) do
+        if set[k] ~= flags[k] then return false end
     end
+    return true
 end
 
---remapKey({}, 104, handleKana())
+local kana = false
+local function handleEvent(e)
+   local keyCode = e:getKeyCode()
+   local flags = e:getFlags()
+   local keyUp = (e:getType() == hs.eventtap.event.types.keyUp)
+   local result = false
+   if keyCode == VK_JIS_KANA then
+      if kana then
+	 --if keyUp then
+	 --   hs.eventtap.keyStroke({}, VK_JIS_EISUU)
+	 --end
+	 --result = true
+	 e:setKeyCode(VK_JIS_EISUU)
+      end
+      if keyUp then
+	 kana = not kana
+      end
+   elseif keyCode == VK_JIS_YEN then
+        if flagsMatches(flags, {'alt'}) then
+            e:setFlags({})
+        elseif flagsMatches(flags, {}) then
+            e:setFlags({alt=true})
+        end
+   end
+   return result
+end
+
+eventtap = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp}, handleEvent)
+eventtap:start()
+
+local log = hs.logger.new("init", "debug")
+
+local function keyCode(modifiers, key)
+   modifiers = modifiers or {}
+   key = string.lower(key)
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, key, true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, key, false):post()
+      log.i(key)
+   end
+end
+
+local function spotlight()
+   local sc = [[
+tell application "System Events"
+    key code 103 using control down
+end tell
+]]
+   hs.osascript.applescript(sc)
+   log.i(sc)
+end
+
+--hs.hotkey.bind({}, "f11", spotlight, nil, nil)
